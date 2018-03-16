f63106dde48cbc187a0759573bb770c83a369ec0
xry <2847779149@qq.com>
2018-03-16 10:10:30 +0800

    test modify file

--- src/main/java/com/xry/studygit/controller/GitTestController.java
+++ src/main/java/com/xry/studygit/controller/GitTestController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/git")
 public class GitTestController {
 
+//    @RequestMapping("/learn")
+//    public String gitLearn(){
+//        return "good good study,day day up!";
+//    }
+
     @RequestMapping("/learn")
     public String gitLearn(){
         return "good good study,day day up!";
